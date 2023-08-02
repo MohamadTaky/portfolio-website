7fa055dea429586ff37ac574bbb774ebb6da673f
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-08-02 22:00:22 +0200

    contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,58 @@
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import Section from "@/components/Section";
+import { GithubIcon, LinkedinIcon, PhoneCallIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function ContactPage() {
-	return <main>Contact</main>;
+  return (
+    <Section>
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+        <div className="flex flex-col items-stretch gap-4">
+          <h2 className="w-fit border-b-2 border-black pb-2 text-3xl font-semibold">Contact me</h2>
+          <Input placeholder="Email" id="email" />
+          <Input placeholder="Name" id="name" />
+          <textarea className="scrollbar-rounded-md min-h-[200px] resize-none rounded-md border-none outline-none !ring-2 ring-black/50 scrollbar-thin focus:ring-black" />
+          <Button className="w-32">Send</Button>
+        </div>
+        <div className="space-y-4">
+          <h2 className="w-fit border-b-2 border-black pb-2 text-3xl font-semibold">Links</h2>
+          <ul className="flex flex-col items-start justify-center gap-8">
+            <li>
+              <Link
+                className="group flex items-center gap-2"
+                target="_blank"
+                href="https://wa.me/+963959953032"
+              >
+                <PhoneCallIcon className="box-content inline h-5 w-5 rounded-full bg-black/80 fill-current p-3 text-gray-500 transition group-hover:text-green-500" />
+                +963959953032
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="group flex items-center gap-2"
+                target="_blank"
+                href="https://github.com/MohamadTaky"
+              >
+                <GithubIcon className="box-content inline h-5 w-5 rounded-full bg-black/80 fill-current p-3 text-gray-500 transition group-hover:text-white" />
+                Github
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="group flex items-center gap-2"
+                target="_blank"
+                href="https://www.instagram.com/mohamad__taky/"
+              >
+                <div className="box-content inline rounded-full bg-black/80 p-3">
+                  <LinkedinIcon className="h-5 w-5 fill-current text-gray-500 transition group-hover:text-blue-800" />
+                </div>
+                Linked in
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Section>
+  );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,21 +4,21 @@ type InputProps = ComponentProps<"input">;
 
 export default function Input({ id, placeholder }: InputProps) {
   return (
-    <div className="relative isolate">
+    <div className="relative isolate rounded-md bg-white ring-2 ring-black/50 transition-shadow  focus-within:ring-black">
       <input
         id={id}
         placeholder={placeholder}
-        className="peer rounded-md bg-white px-3 py-1.5 outline-none ring-2 ring-black/50 transition-shadow placeholder:opacity-0 focus:ring-black"
+        className="peer w-full border-none bg-transparent outline-none placeholder:opacity-0 focus:ring-0"
         type="text"
       />
       {placeholder && (
         <label
-          className="absolute left-3 top-1/2 z-10 -translate-y-8 text-sm font-semibold transition-all text-black/50
-          peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:font-normal peer-placeholder-shown:text-black/50
-          peer-focus:-translate-y-8 peer-focus:text-sm peer-focus:font-semibold peer-focus:text-black"
+          className="pointer-events-none absolute left-3 top-1/2 z-10 origin-left -translate-y-9 scale-75 font-semibold text-black/50 transition-all
+          peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:font-normal peer-placeholder-shown:text-black/50
+          peer-focus:-translate-y-9 peer-focus:scale-75 peer-focus:font-semibold peer-focus:text-black"
           htmlFor={id}
         >
-          <div className="absolute bottom-0.5 left-1/2 h-2 w-[130%] -translate-x-1/2 rounded-md bg-white" />
+          <div className="absolute bottom-0.5 left-1/2 h-[50%] w-[130%] -translate-x-1/2 rounded-md bg-white" />
           <span className="relative">{placeholder}</span>
         </label>
       )}
